abb6f6759a76e50c768bcd69c0a6f81be44bdf0a
Mo Minh <lamminhle2004@gmail.com>
2025-08-15 23:03:47 +0700

    Update state for rag and build simple retrieval

--- src/sub_graph/graph.py
+++ src/sub_graph/graph.py
@@ -1,12 +1,67 @@
 # Viết RAG trong này
 from typing import List, Any, Dict
-from utils.utils import config
+from utils.utils import config, PATH_DB
+from src.sub_graph.states import State
+from chromadb import PersistentClient
+from sentence_transformers import SentenceTransformer
+from pathlib import Path
+from langgraph.graph import StateGraph
 
 EMBEDDING_MODEL = config["retriever"]["embedding_model"]
 TOP_K = config["retriever"]["top_k"]
 COLLECTION_NAME = config["retriever"]["collection_name"]
 
-async def vector_search(query: str) -> List[Dict[str, Any]]:
+
+def start(data: State):
+    return {
+        "input": "Chiến thắng Bạch Đằng vào năm nào?",
+    }
     pass
 
+
+def generate_query(data: State):
+    return {'query': data['input']}
+    pass
+
+
+def retrieval(data: State):
+    client = PersistentClient(path=PATH_DB)
+    collection = client.get_collection(COLLECTION_NAME)
+    model = SentenceTransformer(EMBEDDING_MODEL)
+    embedding = model.encode([data['query']], convert_to_numpy=True)
+    result = collection.query(
+        query_embeddings=embedding.tolist(),
+    )
+    return {
+        'result': result
+    }
+    pass
+
+
+def rerank(data: State):
+    pass
+
+
+def end(data: State):
+    pass
+
+
 graph = None
+
+if __name__ == '__main__':
+    builder = StateGraph(State)
+    builder.add_node("start", start)
+    builder.add_node("generate_query", generate_query)
+    builder.add_node("retrieval", retrieval)
+    builder.add_node("rerank", rerank)
+    builder.add_node("end", end)
+
+    builder.add_edge("start", "generate_query")
+    builder.add_edge("generate_query", "retrieval")
+    builder.add_edge("retrieval", "rerank")
+    builder.add_edge("rerank", "end")
+
+    builder.set_entry_point("start")
+    builder.set_finish_point("end")
+    app = builder.compile()
+    result = app.invoke({})
---
+++ src/sub_graph/states.py
@@ -0,0 +1,7 @@
+from typing import TypedDict, Any
+
+class State(TypedDict):
+    input: str
+    output: str
+    query: str
+    result: Any
